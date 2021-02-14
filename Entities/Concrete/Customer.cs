85660e9f9f11700f9a5c95cae9cb7f98537eb356
burkkca <bur4k111@gmail.com>
2021-02-14 18:58:04 +0300

    14.02.2021

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        [Key]
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
